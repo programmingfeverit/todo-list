2b3c074e49776d6ac1fba9df8e4e1dbfd5f11c8d
Tahmid Labib <tbhlabib1@gmail.com>
2022-07-12 20:10:38 +0600

    Fixed issue Status Changing Issue while in filter

--- src/App.js
+++ src/App.js
@@ -4,22 +4,48 @@ import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 
 function App() {
+  const [firstRender, setFirstRender] = useState(true);
   const [todo, setTodo] = useState("");
   const [todoList, setTodoList] = useState([]);
-  const [firstRender, setFirstRender] = useState(true);
-  useEffect(() => {
+  const [filteredTodos, setFilteredTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+
+  const filterTodos = (sts) => {
     const todos = JSON.parse(localStorage.getItem("todo"));
     if (todos) {
-      setTodoList(todos);
+      switch (sts) {
+        case "completed":
+          setFilteredTodos(todos.filter((todo) => todo.completed === true));
+          break;
+        case "pending":
+          setFilteredTodos(todos.filter((todo) => todo.completed === false));
+          break;
+        default:
+          setFilteredTodos(todos);
+          break;
+      }
+
+      // setTodoList(todos);
       setFirstRender(false);
     }
+  };
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todo"));
+    setTodoList(todos);
+    filterTodos("all");
   }, []);
 
   useEffect(() => {
     if (!firstRender) {
       localStorage.setItem("todo", JSON.stringify(todoList));
+      filterTodos(status);
     }
   }, [todoList]);
+
+  useEffect(() => {
+    filterTodos(status);
+  }, [status]);
 
   return (
     <div>
@@ -31,8 +57,15 @@ function App() {
         setTodoList={setTodoList}
         todoList={todoList}
         todo={todo}
+        filteredTodos={filteredTodos}
       />
-      <TodoList todoList={todoList} setTodoList={setTodoList} />
+
+      <TodoList
+        todoList={todoList}
+        setTodoList={setTodoList}
+        setStatus={setStatus}
+        filteredTodos={filteredTodos}
+      />
     </div>
   );
 }
